b11bca4112f78df329cef3d13b927385f7911fbb
KOLIMI HEENA KOUSAR <heenakousar12354@gmail.com>
2026-06-11 14:55:59 +0530

    Add files via upload

---
+++ Day13/Questions/IBMParking.java
@@ -0,0 +1,28 @@
+package Day13.Questions;
+
+public class IBMParking {
+
+        public static void main(String[] args){
+            System.out.println(parking(new int[]{1,0,0,0,1,0,1}));
+        }
+        public static int parking(int[] arr)
+        {
+            int max=0;
+            int prev=-1;
+
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i]==1){
+                    if(prev==-1){
+                        max=i;
+                    }
+                    max = Math.max(max, (i - prev) / 2);
+                    prev=i;
+                }
+
+            }
+            max=Math.max(max,(arr.length-1-prev));
+            return max;
+        }
+    }
+
+
