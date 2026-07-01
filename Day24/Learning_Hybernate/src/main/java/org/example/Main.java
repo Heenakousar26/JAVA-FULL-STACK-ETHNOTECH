10486edce44ed37c3e2b3837e0aece33d6510664
KOLIMI HEENA KOUSAR <heenakousar12354@gmail.com>
2026-07-01 10:33:41 +0530

    Add files via upload

---
+++ Day24/Learning_Hybernate/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        SessionFactory fac=new Configuration().configure().buildSessionFactory();
+        Session session= fac.openSession();
+        Students st=new Students("Arun",99);
+        session.beginTransaction();
+        session.persist(st);
+        session.getTransaction().commit();
+        session.close();
+        fac.close();
+    }
+}
